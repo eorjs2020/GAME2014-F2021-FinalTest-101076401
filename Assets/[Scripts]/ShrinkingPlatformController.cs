a476577e66e914fdd8ba0d0f174f6ed287c3a7e6
lee <eorjs2020@gmail.com>
2021-12-17 14:36:44 -0500

    Making Shrinking Platform Behavior TODO : smooth Floating

--- Assets/[Scripts]/ShrinkingPlatformController.cs
+++ Assets/[Scripts]/ShrinkingPlatformController.cs
@@ -1,18 +1,65 @@
+//  Daekeone Lee 101076401
+//  ShrinkingPlatformController.cs
+//  Last Update 2021-12-17
+//  Shrinking Platform Behavior
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShrinkingPlatformController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public bool isActive;
+    public float platformTimer;
+    public float threshold;
+
+    private Vector3 scale;
+
+    private void Start()
     {
-        
+        scale = transform.localScale;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        if(isActive)
+        {
+            Shrinking();
+        }
+        else
+        {
+            DeShrinking();
+        }
+
+    }
+    private void Reset()
+    {
+        scale.x = 1;
+    }
+
+    void Shrinking()
+    {        
+        if (scale.x > 0)
+        {
+            scale.x -= Time.deltaTime;
+            transform.localScale = scale;
+        }
+        else
+        {
+            scale.x = 0;
+        }
+    }
+    void DeShrinking()
+    {
+        if(scale.x < 1)
+        {
+            scale.x += Time.deltaTime;
+            transform.localScale = scale;
+        }
+        else
+        {
+            scale.x = 1;
+        }
     }
 }
